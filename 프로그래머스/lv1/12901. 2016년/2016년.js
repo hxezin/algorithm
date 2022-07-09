1007a5a4b4200a3127e6d5f5ea12246b2174ca01
hxezin <102649010+hxezin@users.noreply.github.com>
2022-07-10 02:50:54 +0900

    [level 1] Title: 2016년, Time: 0.10 ms, Memory: 30.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,5 @@
+function solution(a, b) {
+    const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    let dayOfWeek = week[new Date(`2016-${a}-${b}`).getDay()];
+    return dayOfWeek;
+}
