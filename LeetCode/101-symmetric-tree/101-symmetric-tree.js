0ec4c367e0517ffeb7770f867b32b99abe587a4c
hxezin <102649010+hxezin@users.noreply.github.com>
2022-07-10 11:44:27 +0900

    Time: 77 ms (86.20%), Space: 44.8 MB (35.31%) - LeetHub

---
+++ LeetCode/101-symmetric-tree/101-symmetric-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    let traverse = (node1, node2) => {
+        if(node1 === null && node2 === null){
+            return true;
+        }
+        if(node1 === null || node2 === null){
+            return false;
+        }
+        if(node1.val !== node2.val){
+            return false;
+        }
+        return (node1.val === node2.val)
+        && traverse(node1.left, node2.right)
+        && traverse(node1.right, node2.left)
+    }
+    
+    return traverse(root.left, root.right)
+};
