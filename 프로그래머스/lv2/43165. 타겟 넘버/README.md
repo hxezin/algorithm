43356f2ee8ad96bddb01cd28ac24fa4a9d637624
hxezin <102649010+hxezin@users.noreply.github.com>
2022-07-12 00:26:31 +0900

    [level 2] Title: 타겟 넘버, Time: 1.81 ms, Memory: 31.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 32.6 MB, 시간: 1.89 ms
+메모리: 31.9 MB, 시간: 1.81 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -2,14 +2,14 @@ function solution(numbers, target) {
     let count = 0;
     
     const dfs = (idx, sum) => {
-        if(idx === numbers.length){
+        if(idx < numbers.length){
+            dfs(idx+1, sum + numbers[idx])
+            dfs(idx+1, sum - numbers[idx])   
+        }else{
             if(sum === target){
                 count++;
             }
-            return;
         }
-        dfs(idx+1, sum + numbers[idx])
-        dfs(idx+1, sum - numbers[idx])
     }
     
     dfs(0, 0)
