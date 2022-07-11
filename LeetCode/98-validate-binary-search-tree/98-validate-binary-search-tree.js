f3aea5f1a50d6fc351978949a341a5a0215df3ea
hxezin <102649010+hxezin@users.noreply.github.com>
2022-07-11 16:17:29 +0900

    Time: 93 ms (68.81%), Space: 47.2 MB (11.11%) - LeetHub

---
+++ LeetCode/98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+    let data = [];
+    
+    let traverse = (node) => {
+        if(node.left) traverse(node.left)
+        data.push(node.val)
+        if(node.right) traverse(node.right)
+    }
+    
+    traverse(root)
+
+    for(let i = 0; i < data.length; i++){
+        if(data[i] >= data[i+1]){
+            return false;
+        }
+    }
+    
+    return true;
+};
