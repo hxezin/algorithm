2d4c0f89a64bf637d9690eee1b4a4e2c500092ab
hxezin <102649010+hxezin@users.noreply.github.com>
2022-07-14 08:55:05 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.68 ms, Memory: 29.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,12 @@
+function solution(A,B){
+    A.sort((a,b) => a - b)
+    B.sort((a,b) => b - a )
+    
+    let result = 0;
+    
+    for(let i = 0; i < A.length; i++){
+        result += A[i] * B[i]    
+    }
+    
+    return result
+}
