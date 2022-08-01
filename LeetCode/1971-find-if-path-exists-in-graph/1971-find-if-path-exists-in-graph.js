ed7676bab90c37cd79d6d098218f6acc0bb2e06b
hxezin <102649010+hxezin@users.noreply.github.com>
2022-08-01 19:23:42 +0900

    Time: 639 ms (83.08%), Space: 113.9 MB (88.66%) - LeetHub

---
+++ LeetCode/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    const graph = Array.from({length: n}, () => [])
+    
+    edges.forEach(el => {
+        graph[el[0]].push(el[1])
+        graph[el[1]].push(el[0])
+    })
+    
+    const visited = Array(n).fill(0)
+    
+    const temp = []
+    const queue = [];
+    queue.push(source)
+    
+    while(queue.length){
+        let curNode = queue.shift();
+        
+        if(visited[curNode] === 1) continue;
+        
+        visited[curNode] = 1;
+        temp.push(curNode)
+        
+        graph[curNode].forEach(nextNode => {
+            queue.push(nextNode)
+        })
+    }
+    
+    return temp.includes(destination) ? true : false
+};
