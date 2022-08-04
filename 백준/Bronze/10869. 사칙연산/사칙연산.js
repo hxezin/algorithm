746f9ff7476d18dbe8ecbdc9b07ea69c3e3ab4e9
hxezin <102649010+hxezin@users.noreply.github.com>
2022-08-04 11:36:38 +0900

    [Bronze V] Title: 사칙연산, Time: 152 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+const [A, B] = input;
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(Math.floor(A / B));
+console.log(A % B);
