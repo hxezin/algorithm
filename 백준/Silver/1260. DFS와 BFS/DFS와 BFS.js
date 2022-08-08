d1b67a28d608bd025efd8b5e5ea6d6c5ca4a60d6
hxezin <102649010+hxezin@users.noreply.github.com>
2022-08-08 19:43:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 184 ms, Memory: 15164 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -2,54 +2,52 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [N, M, V] = input.shift().split(" ").map(Number);
-input = input.map((el) => el.split(" "));
+const [N, M, V] = input.shift().split(" ").map(Number);
+const nums = input.map((el) => el.split(" "));
 
 let graph = Array.from({ length: N + 1 }, () => []);
 
-for (let el of input) {
+nums.forEach((el) => {
   graph[el[0]].push(el[1]);
   graph[el[1]].push(el[0]);
-}
+});
 
 graph.forEach((el) => el.sort((a, b) => a - b));
 
 const visited = Array(N + 1).fill(0);
-const resultDfs = [];
+const dfsResult = [];
 
 const dfs = (curNode) => {
-  if (visited[curNode]) return;
+  if (visited[curNode] === 1) return;
   visited[curNode] = 1;
-  resultDfs.push(curNode);
+  dfsResult.push(curNode);
 
-  for (let nextNode of graph[curNode]) {
+  graph[curNode].forEach((nextNode) => {
     if (visited[nextNode] === 0) {
       dfs(nextNode);
     }
-  }
+  });
 };
 
 dfs(V);
 
-const resultBfs = [];
-const queue = [];
-queue.push(V);
+console.log(dfsResult.join(" "));
 
 visited.fill(0);
+const bfsResult = [];
+let queue = [];
+queue.push(V);
 
 while (queue.length) {
   let curNode = queue.shift();
-
   if (visited[curNode] === 1) continue;
-
   visited[curNode] = 1;
-  resultBfs.push(curNode);
+  bfsResult.push(curNode);
 
-  for (let nextNode of graph[curNode]) {
+  graph[curNode].forEach((nextNode) => {
     if (visited[nextNode] === 0) {
       queue.push(nextNode);
     }
-  }
+  });
 }
-
-console.log(`${resultDfs.join(" ")}\n${resultBfs.join(" ")}`);
+console.log(bfsResult.join(" "));
