d9eb526dca2603cb31206a44e9caa061ad69bdf6
hxezin <102649010+hxezin@users.noreply.github.com>
2022-08-16 09:07:23 +0900

    [Bronze V] Title: A+B - 4, Time: 152 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const result = input.map((el) => {
+  return el
+    .split(" ")
+    .map(Number)
+    .reduce((a, b) => a + b);
+});
+
+console.log(result.join("\n"));
