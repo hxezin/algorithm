b961d1780db3dedb0c801a34f24bfbd901fa84f3
hxezin <102649010+hxezin@users.noreply.github.com>
2022-08-29 19:30:32 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 30.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,18 +1,19 @@
 function solution(N, stages) {
-    let failRate = [];
-    let failure = Array(N+1).fill(0);
     let user = stages.length;
     
-    for(let i = 0; i < stages.length; i++){
+    // 스테이지별 실패한 사람 수
+    const failure = Array(N+1).fill(0);
+    for(let i = 0; i < user; i++){
         failure[stages[i]]++;
     }
-
-    for(let i = 1; i <= N + 1; i++){
-        failRate.push([i, failure[i] / user])
+    
+    // 실패율
+    let failureRate = [];
+    for(let i = 1; i < N + 1; i++){
+        failureRate.push([i, failure[i] / user])
         user -= failure[i]
     }
     
-    failRate.pop()
-    failRate.sort((a, b) => b[1] - a[1])
-    return failRate.map(rate => rate[0])
+    failureRate.sort((a, b) => b[1] - a[1])
+    return failureRate.map(stage => stage[0])
 }
