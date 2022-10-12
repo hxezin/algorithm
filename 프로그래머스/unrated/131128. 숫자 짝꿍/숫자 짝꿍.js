62c6230fe0e4fb9d61e910afa7a15e1d22e40c46
hxezin <102649010+hxezin@users.noreply.github.com>
2022-10-12 20:20:13 +0900

    [unrated] Title: 숫자 짝꿍, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,22 @@
+function solution(X, Y) {
+    const num = Array.from({length: 10}, () => [0, 0]);
+
+    X.split("").forEach(el => {
+        num[el][0] = num[el][0] + 1 || 1;
+    })
+    
+    Y.split("").forEach(el => {
+          num[el][1] = num[el][1] + 1 || 1;
+    })
+    
+    let resultTemp = ""
+    num.forEach((el, index) => {
+        if(el[0] === 0 || el[1] === 0) return;
+        const repeatNum = Math.min(...el);
+        resultTemp += String(index).repeat(repeatNum)
+    })
+    
+    const result = resultTemp.split("").sort((a, b) => b - a).join("");
+    if(result[0] === "0") return "0";
+    return result.length === 0 ? "-1" : result;
+}
