ed1f2e055fdb6febea33c52caae623efd4446411
hxezin <102649010+hxezin@users.noreply.github.com>
2022-10-24 13:42:40 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.48 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,23 +1,27 @@
 function solution(survey, choices) {
-    const types = ["RT", "CF", "JM", "AN"]
-    const score = {}
+    const types = ["RT", "CF", "JM", "AN"];
+    const score = {};
+    let result = ""
     
+    // 유형 점수 초기화
     types.forEach(type => {
-        const [left, right] = type
+        const [left, right] = type;
         score[left] = 0;
         score[right] = 0;
     })
     
-    choices.forEach((choice, idx) => {
-        const [left, right] = survey[idx]
-        score[choice < 4 ? left : right] += Math.abs(choice - 4)
+    // 선택지 점수 4점 미만일 때 왼쪽 유형에 점수 추가
+    // 선택지 점수 4점 이상일 때 오른쪽 유형에 점수 추가
+    // 점수 = |선택지 점수 - 4|
+    choices.forEach((choice, index) => {
+        const [left, right] = survey[index];
+        score[choice <  4 ? left : right] += Math.abs(choice - 4)
     })
     
-    const result = []
     types.forEach(type => {
-        const [left, right] = type
-        result.push(score[left] >= score[right] ? left : right)
+        const [left, right] = type;
+        result += score[left] >= score[right] ? left : right;
     })
-
-    return result.join("")
+    
+    return result;
 }
