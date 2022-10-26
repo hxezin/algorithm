c6dde5d093a3697d4b81ae6649fd1c909787836a
hxezin <102649010+hxezin@users.noreply.github.com>
2022-10-26 13:23:05 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,23 +1,28 @@
 function solution(id_list, report, k) {
     const graph = {};
-    id_list.forEach(el => {
-        graph[el] = new Set()
+    
+    id_list.forEach(id => {
+        graph[id] = new Set();
     })
     
+    // graph = {유저 id: {해당 유저를 신고한 id}}
     report.forEach(el => {
-        const [a, b] = el.split(" ");
-        graph[b].add(a)
+        const [user1, user2] = el.split(" ");
+        graph[user2].add(user1)
     })
+
+    // 정지 메일 받은 횟수
+    const mailCount = Array(id_list.length).fill(0)
     
-    const count = Array(id_list.length).fill(0);
-    for(let key of Object.keys(graph)){
-        if(graph[key].size >= k){
-            graph[key].forEach(el => {
-                count[id_list.indexOf(el)]++;
+    // 신고 횟수가 k 이상일 경우
+    // 해당 유저를 신고한 유저에게 메일 발송
+    Object.keys(graph).forEach(id => {
+        if(graph[id].size >= k){
+            graph[id].forEach(el => {
+                mailCount[id_list.indexOf(el)]++;
             })
         }
-    }
+    })
     
-    return count
-    
+    return mailCount;
 }
