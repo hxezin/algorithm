bfef01d534a697f51ec02110fda3355e3eb30f59
hxezin <102649010+hxezin@users.noreply.github.com>
2022-11-23 13:52:19 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,29 +1,18 @@
 function solution(n, words) {
-    const result = [];
-    let rounds = 1;
-    let count = 1;
-    let firstLetter = "";
-    let visited = [];
+    let start = words[0][words[0].length - 1]
+    const speak = [];
     
-    for(let i = 0; i < words.length; i++){
-        if(count > n){
-            count = 1;
-            rounds++;
-        }
+    for(let i = 0; i < words.length; i++){     
+        const first = words[i][0]
+        const last = words[i][words[i].length - 1]
         
-        if(i !== 0 && words[i][0] !== firstLetter || visited.includes(words[i])){
-            result.push(count, rounds)
-            break;
+        if(i !== 0 && start !== first || speak.includes(words[i])){
+            return [i % n + 1, Math.floor(i / n) + 1]
         }
-        
-        firstLetter = words[i][words[i].length - 1];
-        visited.push(words[i])
-        count++;
+    
+        start = last
+        speak.push(words[i])
     }
     
-    if(result.length === 0) {
-        return [0, 0]
-    }
-    
-    return result;
+    return [0, 0]
 }
