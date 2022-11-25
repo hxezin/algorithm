e2428cff582bd12761791bfad36c1b332aa4fdc9
hxezin <102649010+hxezin@users.noreply.github.com>
2022-11-25 22:56:49 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,12 +1,10 @@
 function solution(food) {
     let left = ""
-    const right = [];
     
     for(let i = 1; i < food.length; i++){
         const temp = Math.floor(food[i] / 2);
         left += String(i).repeat(temp)
-        right.push(String(i).repeat(temp))
     }
     
-    return `${left}0${right.reverse().join("")}`
+    return `${left}0${[...left].reverse().join("")}`
 }
