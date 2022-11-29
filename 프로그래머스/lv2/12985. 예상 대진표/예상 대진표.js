f73f7958a16e7d4abaef1fb7210ac67108ed1146
hxezin <102649010+hxezin@users.noreply.github.com>
2022-11-29 22:46:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n,a,b)
+{
+    let count = 0;
+    
+    while(a !== b){
+        a = Math.ceil(a / 2)
+        b = Math.ceil(b / 2)
+        
+        count++;
+    }
+    
+    return count;
+}
