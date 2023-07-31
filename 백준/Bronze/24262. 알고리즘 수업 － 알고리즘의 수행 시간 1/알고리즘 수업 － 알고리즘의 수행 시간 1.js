d46ad481f779367e76e10c8c30c7d68523a83681
Hyejin Lee <102649010+hxezin@users.noreply.github.com>
2023-07-31 11:37:06 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 152 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+console.log(1);
+console.log(0);
