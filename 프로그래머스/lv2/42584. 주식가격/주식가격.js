63509e48ea385892c989c68afc7a9a09a432a16a
Hyejin Lee <leehxejin@gmail.com>
2023-09-28 21:49:24 +0900

    [level 2] Title: 주식가격, Time: 3.81 ms, Memory: 42.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -0,0 +1,20 @@
+function solution(prices) {
+    const n = prices.length;
+    const answer = new Array(n).fill(0);
+    const stack = [];
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+            const top = stack.pop();
+            answer[top] = i - top;
+        }
+        stack.push(i);
+    }
+
+    while (stack.length > 0) {
+        const top = stack.pop();
+        answer[top] = n - 1 - top;
+    }
+
+    return answer;
+}
