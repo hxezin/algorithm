0d38ea1dd365127605083bb84c15c9aec0dd59cc
Hyejin Lee <leehxejin@gmail.com>
2023-10-16 16:57:56 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 9588 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -7,11 +7,14 @@ const str = input[1];
 
 const r = 31;
 const M = 1234567891;
+
 let hash = 0;
+let rPower = 1;
 
 for (let i = 0; i < L; i++) {
   const charCode = str.charCodeAt(i) - 96;
-  hash = (hash + charCode * r ** i) % M;
+  hash = (hash + charCode * rPower) % M;
+  rPower = (rPower * r) % M;
 }
 
 console.log(hash);
