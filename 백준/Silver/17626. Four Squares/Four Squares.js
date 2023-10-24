d993da573f882f1dd2926ed30848cb8b6b5e5964
Hyejin Lee <leehxejin@gmail.com>
2023-10-24 13:20:09 +0900

    [Silver III] Title: Four Squares, Time: 224 ms, Memory: 10816 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
+
+const n = Number(input);
+
+const dp = new Array(n + 1).fill(Infinity);
+dp[0] = 0;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+  }
+}
+
+console.log(dp[n]);
