5fa133afb5e11c9a33dceb32ab8ee2a0bf14f708
Hyejin Lee <leehxejin@gmail.com>
2023-10-31 23:43:33 +0900

    [Silver I] Title: IOIOI, Time: 192 ms, Memory: 12108 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -5,23 +5,21 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const [N, M] = input.slice(0, 2).map(Number);
 const S = input[2];
 
-let count = 0;
+let result = 0;
 
-for (let i = 0; i < M - 2 * N; i++) {
-  if (S[i] === 'I' && S[i + 1] === 'O' && S[i + 2] === 'I') {
-    let valid = true;
+for (let i = 0; i < M; i++) {
+  let count = 0;
 
-    for (let j = 3; j < 2 * N; j += 2) {
-      if (S[i + j] !== 'O' || S[i + j + 1] !== 'I') {
-        valid = false;
-        break;
-      }
-    }
-
-    if (valid) {
+  if (S[i] === 'I') {
+    while (S[i + 1] === 'O' && S[i + 2] === 'I') {
       count++;
+      if (count === N) {
+        result++;
+        count--;
+      }
+      i += 2;
     }
   }
 }
 
-console.log(count);
+console.log(result);
