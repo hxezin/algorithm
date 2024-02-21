62fd5313e9f74d376f06236327caae6fdc2cf96e
Hyejin Lee <leehxejin@gmail.com>
2024-02-21 23:54:08 +0900

    [Bronze I] Title: 세로읽기, Time: 136 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.js
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const result = [];
+const result = Array.from({ length: 15 }, () => '');
 
-for (let i = 0; i < 5; i++) {
-  const word = input[i].split('');
-  let idx = 0;
-  while (idx < word.length) {
-    result[idx] = (result[idx] || '') + word[idx];
-    idx++;
-  }
-}
+input.forEach((word) => {
+  word.split('').forEach((char, idx) => {
+    result[idx] += char;
+  });
+});
 
 console.log(result.join(''));
