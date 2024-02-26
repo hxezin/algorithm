fa7251bdb8152d9e2eb15746b6da7cbd3aed3f3b
Hyejin Lee <leehxejin@gmail.com>
2024-02-26 11:52:54 +0900

    [Bronze II] Title: 진법 변환, Time: 196 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+console.log(parseInt(input[0], input[1]));
