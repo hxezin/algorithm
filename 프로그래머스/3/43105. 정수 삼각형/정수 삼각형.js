55aac6235c68d08f852c4fe71ad6c28d4f6185d0
이혜진 Hyejin Lee <leehxejin@gmail.com>
2025-06-11 21:29:46 +0900

    [level 3] Title: 정수 삼각형, Time: 5.83 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,15 @@
+function solution(triangle) {
+    var answer = 0;
+    let height = triangle.length;
+    
+    for(let i = height - 2; i >= 0; i--){
+        for(let j = 0; j < triangle[i].length; j++){
+            triangle[i][j] += Math.max(
+                triangle[i + 1][j],
+                triangle[i + 1][j + 1]
+            )
+        }
+    }
+    
+    return triangle[0][0];
+}
